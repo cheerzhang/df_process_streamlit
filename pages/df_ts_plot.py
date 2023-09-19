3b7f4fbf2121a16e01687e72d50e8e5dda697846
cheerzhang <zhanglenus@gmail.com>
2023-09-19 11:12:47 +0200

    add plot if gender

--- pages/df_ts_plot.py
+++ pages/df_ts_plot.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import pandas as pd
-from util import process_util
+from util import model_util, process_util
 
 def app():
     st.markdown("# Display TimeSeries DataFrame")
     time_tool = process_util.Time_Tool()
+    model_obj = model_util.GetModelPredict()
     upload_df_1 = st.file_uploader("Choose 1st data file:", key="file1_upload")
     if upload_df_1 is not None:
         df1 = pd.read_csv(upload_df_1)
@@ -30,6 +31,34 @@ def app():
             with col_right:
                 st.dataframe(day_user_counts)
             st.success(f"procesed, time using: {time_tool.end_timer()}")
+        # plot gender f, m ratio
+        st.divider()
+        fix_gender = st.checkbox("Fix gender by firstname")
+        if fix_gender:
+            first_name_column = st.selectbox('Select First Name Column', df1.columns.values)
+            df_gender = df1[[first_name_column]]
+            model_obj.set_url("http://16.170.214.92:5002/api/predict_gender_v1")
+            df_gender = model_obj.call_gender_model(df_gender, first_name_column)
+            df1['gender'] = df_gender['pred']
+            # plot ratio of female everyday
+        else:
+            col_gender_select_left, col_gender_select_right = st.columns(2)
+            with col_gender_select_left:
+                gender_column = st.selectbox('Select Gender Column', df1.columns.values)
+            with col_gender_select_right:
+                gender_f_values = st.multiselect('Values as Female', df1[gender_column].unique())
+                df1['gender'] = df1[gender_column].apply(lambda x: 'F' if x in gender_f_values else 'M')
+        female_user_counts = df1[df1['gender'] == 'F'].groupby('day')['gender'].count().reset_index(name='female_count')
+        total_user_counts = df1.groupby('day')['gender'].count().reset_index(name='total_count')
+        gender_df = total_user_counts.merge(female_user_counts, on='day')
+        gender_df['female_ratio'] = gender_df['female_count'] / gender_df['total_count']
+        gender_df['male_ratio'] = 1 - gender_df['female_ratio']
+        gender_df.set_index('day', inplace=True)
+        col_f_left, col_f_right = st.columns([3, 1])
+        with col_f_left:
+            st.bar_chart(gender_df[['female_ratio', 'male_ratio']])
+        with col_f_right:
+            st.dataframe(gender_df[['female_ratio', 'male_ratio', 'female_count', 'total_count']])
 
 
 if __name__ == '__main__':
---
+++ util/model_util.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import requests
+
+class GetModelPredict:
+    def __init__(self, url = None):
+        self.url = url
+    def set_url(self, url):
+        self.url = url
+    def call_gender_model(self, df, first_name=None):
+        if first_name is not None:
+            df['first_name'] = df[first_name]
+        df['first_name'] = df['first_name'].fillna("")
+        data = df[['first_name']].to_dict(orient='records')
+        api_url = self.url
+        response = requests.post(api_url, json=data)
+        if response.status_code == 200:
+            response_data = response.json()
+            df['pred'] = response_data['Predict of gender']
+            return df
