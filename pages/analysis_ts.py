34f4ae1b4e622e90b7b4eacbcb6bc6480dd8fcd4
cheerzhang <zhanglenus@gmail.com>
2023-09-28 00:04:16 +0200

    Update analysis_ts.py

--- pages/analysis_ts.py
+++ pages/analysis_ts.py
@@ -22,7 +22,7 @@ def app():
         with col_option2:
             st.markdown('#### exxclude values')
             exclude_column = st.selectbox('Select Exclude Column', df.columns.values, index=0)
-            exclude_values = st.multiselect('Exclude Values', df[exclude_column].unique(), [])
+            exclude_values = st.multiselect('Exclude Values', df[exclude_column].unique(), [df[exclude_column].unique()[0]])
         # exclude logic
         df = df[~df[exclude_column].isin(exclude_values)]
         df[time_column] = pd.to_datetime(df[time_column], errors='coerce') 
@@ -32,9 +32,10 @@ def app():
         df_3_true = df[df[target_column] == 1].resample(frequency_dist[frequency_column]).size().reset_index(name='total_true')
         df_3_false = df[df[target_column] == 0].resample(frequency_dist[frequency_column]).size().reset_index(name='total_false')
         df_3_null = df[df[target_column] == -1].resample(frequency_dist[frequency_column]).size().reset_index(name='total_null')
-        df_3 = df_3_true.merge(df_3_false, on='time')
-        df_3 = df_3.merge(df_3_null, on='time')
+        df_3 = df_3_true.merge(df_3_false, on='time', how='outer')
+        df_3 = df_3.merge(df_3_null, on='time', how='outer')
         df_3.set_index('time', inplace=True)
+        st.markdown('Total True, Total False, Total Null counts')
         st.line_chart(df_3[['total_true', 'total_false', 'total_null']])
         # calculate ratio
         df_3['true_in_all'] = df_3['total_true'] / (df_3['total_true'] + df_3['total_false'] + df_3['total_null'])
@@ -50,8 +51,9 @@ def app():
         df_3['null_in_tn'] = df_3['total_null'] / (df_3['total_true'] + df_3['total_null'])
         with col_option2:
             ratio_columns = ['true_in_all', 'true_in_tf', 'true_in_tn', 'false_in_all', 'false_in_tf', 'false_in_fn', 'null_in_all', 'null_in_fn', 'null_in_tn']
-            display_columns = st.multiselect('Select Display Columns', ratio_columns, [])
-        st.line_chart(df_3)
+            display_columns = st.multiselect('Select Display Columns', ratio_columns, ratio_columns)
+        st.markdown('True, False, Null ratio')
+        st.line_chart(df_3[display_columns])
         
         
 
