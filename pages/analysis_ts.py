dbd3e82dc6763d044d2077452e49d6a07b6bd090
cheerzhang <zhanglenus@gmail.com>
2023-09-28 06:57:00 +0200

    Update analysis_ts.py

--- pages/analysis_ts.py
+++ pages/analysis_ts.py
@@ -20,16 +20,16 @@ def app():
             frequency_column = st.selectbox('Select Frequency', ['Week', 'Day', 'Hour'], index=1) 
             target_column = st.selectbox('Select Analysis Column', df.columns.values, index=0)
         with col_option2:
-            st.markdown('#### exxclude values')
+            st.markdown('#### exclude values')
             col_left1, col_right1 = st.columns(2)
             with col_left1:
-                exclude_column1 = st.selectbox('Select Exclude Column 1', df.columns.values, index=0)
-                exclude_values1 = st.multiselect('Exclude Values 1', df[exclude_column1].unique(), [df[exclude_column1].unique()])
+                include_column1 = st.selectbox('Select Exclude Column 1', df.columns.values, index=0)
+                include_values1 = st.multiselect('Exclude Values 1', df[include_column1].unique(), [df[include_column1].unique()])
             with col_right1:
                 exclude_column = st.selectbox('Select Exclude Column 2', df.columns.values, index=0)
                 exclude_values = st.multiselect('Exclude Values 2', df[exclude_column].unique(), [df[exclude_column].unique()[0]])
         # exclude logic
-        df = df[~df[exclude_column1].isin(exclude_values1)]
+        df = df[df[include_column1].isin(include_values1)]
         df[time_column] = pd.to_datetime(df[time_column], errors='coerce') 
         df['time'] = df[time_column]
         df.set_index('time', inplace=True)
