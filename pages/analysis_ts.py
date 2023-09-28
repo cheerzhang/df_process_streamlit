f39823c6ee9457fd8eba1bc4e27d11a3117ea360
cheerzhang <zhanglenus@gmail.com>
2023-09-28 16:01:52 +0200

    Update analysis_ts.py

--- pages/analysis_ts.py
+++ pages/analysis_ts.py
@@ -76,37 +76,8 @@ def app():
         # all values plot:
         after_exclude = [item for item in df_[include_column1].unique() if item not in include_values1]
         display_by_filter(df_, target_column, frequency_column, include_column1, after_exclude)
-        # time series logic - 3 value
-        '''
-        df_3_true = df_[df_[target_column] == 1].resample(frequency_dist[frequency_column]).size().reset_index(name='total_true')
-        df_3_false = df_[df_[target_column] == 0].resample(frequency_dist[frequency_column]).size().reset_index(name='total_false')
-        df_3_null = df_[df_[target_column] == -1].resample(frequency_dist[frequency_column]).size().reset_index(name='total_null')
-        df_3 = df_3_true.merge(df_3_false, on='time', how='outer')
-        df_3 = df_3.merge(df_3_null, on='time', how='outer')
-        df_3.set_index('time', inplace=True)
-        st.markdown('Total True, Total False, Total Null counts')
-        with st.expander('Total True, Total False, Total Null counts'):
-            st.bar_chart(df_3[['total_true', 'total_false', 'total_null']])
-        # calculate ratio
-        df_3['true_in_all'] = df_3['total_true'] / (df_3['total_true'] + df_3['total_false'] + df_3['total_null'])
-        df_3['true_in_tf'] = df_3['total_true'] / (df_3['total_true'] + df_3['total_false'])
-        df_3['true_in_tn'] = df_3['total_true'] / (df_3['total_true'] + df_3['total_null'])
-
-        df_3['false_in_all'] = df_3['total_false'] / (df_3['total_true'] + df_3['total_false'] + df_3['total_null'])
-        df_3['false_in_tf'] = df_3['total_false'] / (df_3['total_true'] + df_3['total_false'])
-        df_3['false_in_fn'] = df_3['total_false'] / (df_3['total_false'] + df_3['total_null'])
-
-        df_3['null_in_all'] = df_3['total_null'] / (df_3['total_true'] + df_3['total_false'] + df_3['total_null'])
-        df_3['null_in_fn'] = df_3['total_null'] / (df_3['total_false'] + df_3['total_null'])
-        df_3['null_in_tn'] = df_3['total_null'] / (df_3['total_true'] + df_3['total_null'])
-        
-        st.markdown('True, False, Null ratio')
-        with st.expander('True, False, Null ratio'):
-            ratio_columns = ['true_in_all', 'true_in_tf', 'true_in_tn', 'false_in_all', 'false_in_tf', 'false_in_fn', 'null_in_all', 'null_in_fn', 'null_in_tn']
-            display_columns = st.multiselect('Select Display Columns', ratio_columns, ratio_columns)
-            st.line_chart(df_3[display_columns])
-            st.bar_chart(df_3[display_columns])
-        '''
+        # logic start --------------------------------------------------------
+    
         st.divider()
         st.markdown(f'### Report by {frequency_column} - {include_column1}')
         df_ = df.copy()
