4bc5efbf54e6164feaf16ff72a5c2450ec12867c
cheerzhang <zhanglenus@gmail.com>
2023-10-24 14:42:56 +0200

    Update df_change_name.py
    fix typo

--- pages/df_change_name.py
+++ pages/df_change_name.py
@@ -3,18 +3,18 @@ import pandas as pd
 
 def app():
     upload_df = st.file_uploader("Choose dataframe file:", key="file1_upload")
-    if upload_df:
+    if upload_df is not None:
         df = pd.read_csv(upload_df)
         col_old_df, col_new_df = st.columns(2)
         with col_old_df:
-            st.datafarme(df)
+            st.dataframe(df)
         original_names = df.columns.values.tolist()
         new_names = {}
         for name in original_names:
             new_names[name] = st.text_input(f'Original Name: {name}', name, key=f"text_input_{name}")
         df_new = df.rename(columns=name)
         with col_new_df:
-            st.datafarme(df_new)
+            st.dataframe(df_new)
 
 
 if __name__ == '__main__':
