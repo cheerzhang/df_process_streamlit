9b947de7f4e0656a6ff89c3afd8b07f3a1a84309
cheerzhang <zhanglenus@gmail.com>
2023-11-27 14:26:40 +0100

    fix logout bug

--- main_page.py
+++ main_page.py
@@ -6,23 +6,21 @@ import yaml
 from yaml.loader import SafeLoader
 with open('config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized']
-)
+    authenticator = stauth.Authenticate(
+        config['credentials'],
+        config['cookie']['name'],
+        config['cookie']['key'],
+        config['cookie']['expiry_days'],
+        config['preauthorized']
+    )
 name, authentication_status, username = authenticator.login('Login', 'main')
 
 
 from page.combine_df import combine_two_df
 from page.psi import get_psi
 
-if st.session_state["authentication_status"]:
-    with st.sidebar:
-        authenticator.logout('Logout', 'main', key='logout')
-        st.write(f'Welcome *{st.session_state["name"]}*')
+if authentication_status:
+    st.write(f'Welcome *{st.session_state["name"]}*')
     tab1, tab2, tab3 = st.tabs(["Combine df", "PSI", "Logout"])
     with tab1:
         st.title('Combine Dataframe')
@@ -33,8 +31,9 @@ if st.session_state["authentication_status"]:
     with tab3:
         authenticator.logout('Logout', 'main')
         st.write(f'Welcome *{st.session_state["name"]}*')
+
     
-elif st.session_state["authentication_status"] == False:
+elif authentication_status == False:
     st.error('Username/password is incorrect')
-elif st.session_state["authentication_status"] == None:
+elif authentication_status == None:
     st.warning('Please enter your username and password')
